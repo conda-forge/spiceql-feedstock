1ada01fdbeeae626f04c17dd0029eac9b6965208
acpaquette <acpaquette@usgs.gov>
2025-07-17 15:42:22 -0700

    Add debug flag for finding packages

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,5 @@ mkdir build && cd build
 # to avoid complaints in conda-forge, see https://conda-forge.org/docs/maintainer/knowledge_base/#newer-c-features-with-old-sdk 
 export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
 cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=RELEASE -DSPICEQL_BUILD_TESTS=OFF  -DPython3_EXECUTABLE=$PYTHON  ..
-cmake --build . --config RELEASE
+cmake --debug-find --build . --config RELEASE
 cmake --install . --config RELEASE
